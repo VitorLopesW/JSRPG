be4c7b753313d90e96dcd82773f0601bb2abc898
VitorLopesW <67442259+VitorLopesW@users.noreply.github.com>
2023-01-04 14:35:16 -0300

    a

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+        console.log('test')
+    }, 0);
+});
+
 startScreen()
 
 function startScreen(){ //inicio do jogo
 let letDisplay = document.getElementById('display') 
-
 
 
 }
