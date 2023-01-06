317d81242998cec877833a0ae01dd0da35d0d7c0
VitorLopesW <67442259+VitorLopesW@users.noreply.github.com>
2023-01-05 22:10:26 -0300

    a

---
+++ objects.js
@@ -0,0 +1,15 @@
+const barbarian = {
+    'imgBig': '<img src="raw arts/barbarian.png">',
+    'imgBigFemale': '<img src="raw arts/barbarian2.png">'
+
+}
+const warrior = {
+    'imgBig': '<img src="raw arts/battlecry.png">',
+    'imgBigFemale': '<img src="raw arts/battlecry2.png">'
+
+}
+const mage = {
+    'imgBig': '<img src="raw arts/mage.png">',
+    'imgBigFemale': '<img src="raw arts/mage2.png">'
+
+}
--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-
-
-
-startScreen()
-
-function startScreen(){ //inicio do jogo
-let letDisplay = document.getElementById('display') 
-
-
+function newGame(){
+let i = document.getElementById('partOne');
+let y = document.getElementById('newGame');
+i.style.display = 'block';
+y.style.display = 'none';
 }
 
 
-function description () {
+function characterInfo(char) {
+    let i = document.getElementById('bigChar')
+    switch(char) {
+        case 'barbarian':
+            classbtnRemove()
+            document.getElementById("barbarianBtn").classList.add('class-btnActive');
+            i.innerHTML = barbarian.imgBig;
+            break;
+        case 'warrior':
+            classbtnRemove()
+            document.getElementById("warriorBtn").classList.add('class-btnActive');
+            i.innerHTML = warrior.imgBig;
+            break;
+        case 'mage':
+            classbtnRemove()
+            document.getElementById("mageBtn").classList.add('class-btnActive');
+            i.innerHTML = mage.imgBig;
+            break;
+    }
 
-    
+
+    function classbtnRemove(){
+        document.getElementById("barbarianBtn").classList.remove('class-btnActive');
+        document.getElementById("warriorBtn").classList.remove('class-btnActive');
+        document.getElementById("mageBtn").classList.remove('class-btnActive');
+    }
 }
+
